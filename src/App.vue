e6ae729cb31e87b6397660a4c7230e56dc9b22ad
binhus <eng.robsonwilli@gmail.com>
2023-04-04 19:32:07 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { FirstComponent } from './src'
+</script>
+
+<template>
+  <FirstComponent msg="Componente teste 1" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
